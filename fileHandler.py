1b8d181ceb4b94287d03084960c8d1f71c0c2d8e
Krieo <krieolin@gmail.com>
2023-10-14 16:25:48 +0200

    updated file handler and made only print first 5 for testing

--- fileHandler.py
+++ fileHandler.py
@@ -1,10 +1,20 @@
 import csv
 
+from ImageData import ImageData
 
+"""
+This method reads the contents of the file
+and stores it in the class and returns a list
+of the class
+"""
 def read_csv_file(file_path):
     data = []
     with open(file_path, mode="r") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            data.append(row)
+            # Create an instance of ImageData for each row in the CSV
+            image_data = ImageData(row['img_fName'], row['img_w'], row['img_h'],
+                                   row['bbx_xtl'], row['bbx_ytl'], row['bbx_xbr'],
+                                   row['bbx_ybr'], row['class_label'])
+            data.append(image_data)
     return data
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import fileHandler
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     file_path = "phase2_train_v0.csv"
-    csv_data = fileHandler.read_csv_file(file_path)
-    i = 0
-    for row in csv_data:
-        print(row)
-        print(i)
-        i += 1
+    image_data_list = fileHandler.read_csv_file(file_path)
 
-
+    for i, image_data in enumerate(image_data_list[:5]):
+        print(f"Image {i + 1}:")
+        print(f"File Name: {image_data.img_fName}")
+        print(f"Width: {image_data.img_w}")
+        print(f"Height: {image_data.img_h}")
+        print(
+            f"Bounding Box (xtl, ytl, xbr, ybr): ({image_data.bbx_xtl}, {image_data.bbx_ytl}, {image_data.bbx_xbr}, {image_data.bbx_ybr})")
+        print(f"Class Label: {image_data.class_label}")
+        print()
